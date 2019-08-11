738a564671b07ad4dbb805a9126b4eac54b3c7e5
Callem Pittard <callem.pittard@gmail.com>
2019-08-11 19:04:55 +1000

    Fix if

--- scripts/03-docker-tag-and-push.sh
+++ scripts/03-docker-tag-and-push.sh
@@ -3,7 +3,7 @@ tag=${$1:-dev}
 echo "$DOCKER_PASS" | docker login --username chips5k --password-stdin
 docker tag chips5k/go-api chips5k/go-api:$tag
 docker push chips5k/go-api:$tag
-if [$tag != 'dev']
+if [[$tag != 'dev']]
 then
     docker tag chips5k/go-api:$tag chips5k/go-api:latest
     docker push chips5k/go-api:latest
