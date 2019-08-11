f6f058619cb76c996bc8f30b2e2cef28f984f422
Callem Pittard <callem.pittard@gmail.com>
2019-08-11 19:26:26 +1000

    Fix scripts again

--- scripts/04-deploy.sh
+++ scripts/04-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-environment = ${1:-development-go-api}
+environment=${1:-"development-go-api"}
 tag=${2:-dev}
 
-echo "Setting deploy tag to ${tag}"
+echo "Setting deploy tag to $tag"
 sed -i.bk -e "s/\${tag}/$tag/" Dockerrun.aws.json
 
 echo "Ensure environment"
 
-eb printenv || eb create ${environment}
+eb printenv $environment || eb create $environment
 
 echo "Deploy to environment"
-eb deploy ${environment}
+eb deploy $environment
