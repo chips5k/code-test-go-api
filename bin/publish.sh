393d305c1e9e4cae7e79f974dd35d82c026a6e80
Callem Pittard <callem.pittard@gmail.com>
2019-08-19 22:14:48 +1000

    try the linting, separate the tagging and pushing

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,6 +2,7 @@
 sha=$CIRCLE_SHA1
 workflowID=$CIRCLE_WORKFLOW_ID
 echo "$DOCKER_PASS" | docker login --username chips5k --password-stdin
-docker tag "chips5k/go-api:$sha" "chips5k/go-api:$workflowID" "chips5k/go-api:latest"
+docker tag "chips5k/go-api:$sha" "chips5k/go-api:$workflowID"
+docker tag "chips5k/go-api:$sha"  "chips5k/go-api:latest"
 docker push "chips5k/go-api:$workflowID"
 docker push "chips5k/go-api:latest"
